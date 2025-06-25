2e88978d787b6aa42185a401a54f74407050313a
michaelabelola <michaelabelola@gmail.com>
2025-06-25 14:11:05 -0400

    first

---
+++ src/main/java/com/suiteonix/schoolpaz/SchoolPazBeApplication.java
@@ -0,0 +1,13 @@
+package com.suiteonix.schoolpaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPazBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPazBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suiteonix/schoolpaz/SchoolPazBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suiteonix.schoolpaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPazBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
